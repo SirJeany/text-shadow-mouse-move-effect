632314dce2dc14b3ee6846e75ba6abd8e6985259
SirJeany <jmvanriet1@gmail.com>
2020-05-08 14:52:56 +0200

    added skeleton for task

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+function shadow(e) {
+    console.log(e);
+}
+
+hero.addEventListener('mousemove', shadow);
